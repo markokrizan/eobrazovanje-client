fbfb2b39d0cadf95b29d5a71038eaf8c30403fb9
Marko Krizan <markokrizan64@gmail.com>
2019-05-23 15:18:13 +0200

    Added pingendo specific dependencies and basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MenuItemComponent } from './components/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainMenuComponent,
+    LoginComponent,
+    RegisterComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
